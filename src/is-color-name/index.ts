f76dbfbb2c975218c37b8e5861cf34d7bef71ef0
Mazel <mazuel.loic@gmail.com>
2020-12-02 13:55:16 +0100

    refactor(is-color-name): use color list instead Option HTMLElement

--- src/is-color-name/index.ts
+++ src/is-color-name/index.ts
@@ -1,7 +1,9 @@
+import colors from './../_constantes'
+
 export default (color: string): boolean => {
   if (!color) throw new Error('is-color-name: No color provided')
 
-  const s = new Option().style
-  s.color = color
-  return s.color === color
+  const colorsList = Object.keys(colors)
+
+  return colorsList.includes(color)
 }
